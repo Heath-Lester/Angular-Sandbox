b0c65fe37f71a1ae21e2ab694672323ccd98605d
Heath Lester <h.allen.lester@gmail.com>
2024-01-27 16:33:32 -0500

    add rxjs and ngrx dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimations(), provideStore()]
 };
