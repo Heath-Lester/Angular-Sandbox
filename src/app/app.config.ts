4b1a90467f0df21958c6cba69e00746f6efba10a
Heath Lester <h.allen.lester@gmail.com>
2024-01-28 12:34:14 -0500

    format files with prettier

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideStore } from '@ngrx/store';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimations(), provideStore()]
+    providers: [provideRouter(routes), provideClientHydration(), provideAnimations(), provideStore()],
 };
